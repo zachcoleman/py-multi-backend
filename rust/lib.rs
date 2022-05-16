26da95301e131c11377c58324e8fcbb7296ba2c5
zachcoleman <zacharywcoleman@gmail.com>
2022-05-16 22:52:25 +0000

    threaded versions

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,9 +1,9 @@
 use numpy::*;
 use pyo3::prelude::*;
 
-/// Rust implementation of binary msk to RLE
+/// Rust implementation of binary mask to RLE
 #[pyfunction]
-fn rust_1D_mask_to_rle(_py: Python<'_>, arr: &PyArray1<bool>) -> Vec<u32> {
+fn arr2rle(_py: Python<'_>, arr: &PyArray1<bool>) -> Vec<u32> {
     let mut counts: Vec<u32> = vec![];
     let mut count: u32 = 0;
     let mut tgt = false;
@@ -22,9 +22,9 @@ fn rust_1D_mask_to_rle(_py: Python<'_>, arr: &PyArray1<bool>) -> Vec<u32> {
     return counts;
 }
 
-/// Rust implementation of binary msk to RLE
+/// Rust implementation of binary mask to RLE
 #[pyfunction]
-fn rust_f_order_2D_mask_to_rle(_py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32> {
+fn f_order_mask2rle(_py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32> {
     let mut counts: Vec<u32> = vec![];
     let mut count: u32 = 0;
     let mut tgt = false;
@@ -45,8 +45,9 @@ fn rust_f_order_2D_mask_to_rle(_py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32
     return counts;
 }
 
+/// Rust implementation of binary mask to RLE
 #[pyfunction]
-fn rust_c_order_2D_mask_to_rle(_py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32> {
+fn c_order_mask2rle(_py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32> {
     let mut counts: Vec<u32> = vec![];
     let mut count: u32 = 0;
     let mut tgt = false;
@@ -65,36 +66,96 @@ fn rust_c_order_2D_mask_to_rle(_py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32
     return counts;
 }
 
+/// Rust implementation of binary mask to RLE
+#[pyfunction]
+fn thread_arr2rle(py: Python<'_>, arr: &PyArray1<bool>) -> Vec<u32> {
+    let arr = arr.readonly();
+    let arr = arr.as_array();
 
-/// Rust implementation of binary msk to RLE
-// #[pyfunction]
-// fn rust_allow_threads_mask_to_rle(py: Python<'_>, arr: &PyArray1<bool>) -> Vec<u32> {
-//     py.allow_threads(move || {
+    py.allow_threads(move || {
+        let mut counts: Vec<u32> = vec![];
+        let mut count: u32 = 0;
+        let mut tgt = false;
 
-//         let mut counts: Vec<u32> = vec![];
-//         let mut count: u32 = 0;
-//         let mut tgt = false;
+        for curr in arr.iter() {
+            if *curr == tgt {
+                count = count + 1;
+            } else {
+                counts.push(count);
+                tgt = *curr;
+                count = 1;
+            }
+        }
 
-//         for curr in arr.readonly().as_array().clone().iter() {
-//             if *curr == tgt {
-//                 count = count + 1;
-//             } else {
-//                 counts.push(count);
-//                 tgt = *curr;
-//                 count = 1;
-//             }
-//         }
+        counts.push(count);
+        return counts;
+    })
+}
 
-//         counts.push(count);
-//         counts
-//     })
-// }
+/// Rust implementation of binary mask to RLE
+#[pyfunction]
+fn thread_f_order_mask2rle(py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32> {
+    let arr = arr.readonly();
+    let arr = arr.as_array();
+
+    py.allow_threads(move || {
+        let mut counts: Vec<u32> = vec![];
+        let mut count: u32 = 0;
+        let mut tgt = false;
+
+        for col in arr.axis_iter(ndarray::Axis(1)) {
+            for curr in col.iter(){
+                if *curr == tgt {
+                    count = count + 1;
+                } else {
+                    counts.push(count);
+                    tgt = *curr;
+                    count = 1;
+                }
+            }
+        }
+
+        counts.push(count);
+        return counts;
+    })
+}
+
+/// Rust implementation of binary mask to RLE
+#[pyfunction]
+fn thread_c_order_mask2rle(py: Python<'_>, arr: &PyArray2<bool>) -> Vec<u32> {
+    let arr = arr.readonly();
+    let arr = arr.as_array();
+
+    py.allow_threads(move || {
+        let mut counts: Vec<u32> = vec![];
+        let mut count: u32 = 0;
+        let mut tgt = false;
+
+        for curr in arr.iter() {
+            if *curr == tgt {
+                count = count + 1;
+            } else {
+                counts.push(count);
+                tgt = *curr;
+                count = 1;
+            }
+        }
+
+        counts.push(count);
+        return counts;
+    })
+}
+
+
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn py_multi_backend(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(rust_1D_mask_to_rle, m)?)?;
-    m.add_function(wrap_pyfunction!(rust_f_order_2D_mask_to_rle, m)?)?;
-    m.add_function(wrap_pyfunction!(rust_c_order_2D_mask_to_rle, m)?)?;
+    m.add_function(wrap_pyfunction!(arr2rle, m)?)?;
+    m.add_function(wrap_pyfunction!(f_order_mask2rle, m)?)?;
+    m.add_function(wrap_pyfunction!(c_order_mask2rle, m)?)?;
+    m.add_function(wrap_pyfunction!(thread_arr2rle, m)?)?;
+    m.add_function(wrap_pyfunction!(thread_f_order_mask2rle, m)?)?;
+    m.add_function(wrap_pyfunction!(thread_c_order_mask2rle, m)?)?;
     Ok(())
 }
